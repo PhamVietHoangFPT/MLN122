572665b24ab57fb42ab73dc75b76ceec7603d83c
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-07-19 15:11:17 +0700

    Add custom server host and allowedHosts to Vite config
    Configured the Vite dev server to accept connections from any host and specifically allow 'ec2-3-0-101-133.ap-southeast-1.compute.amazonaws.com'. This enables development access from external machines or cloud instances.

--- mln122_frontend/vite.config.ts
+++ mln122_frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Thêm cấu hình này
+    host: true, // Tương đương với việc chạy cờ --host
+    allowedHosts: ['ec2-3-0-101-133.ap-southeast-1.compute.amazonaws.com'],
+  },
 })
